cf4db2cca1c354e3b23931805c4813ff27a89bc5
Justin Roller <justin@>
2018-06-08 16:50:55 -0700

    added html and components for login page.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent   {
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit(email:string, password:string) {
+    if(email.length != 0 && password.length != 0) {
+      window.location.href="accounts";
+    }
   }
-
 }
