7c2bbc13ff5893e7aefe4e61f63317d758f0af3c
Justin Roller <justin@>
2018-06-25 10:31:41 -0700

    deployed site.

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Movie } from './movie.model';
-import { MOVIES } from './mock-movies';
+// import { MOVIES } from './mock-movies';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
